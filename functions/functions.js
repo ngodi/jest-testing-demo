c04c7daeb3c6aa5dd7b6113a4ceccefd35337064
ngodi <albertngodi@gmail.com>
2019-11-05 04:12:58 +0100

    added basic calculator functions and tests

--- functions/functions.js
+++ functions/functions.js
@@ -1,5 +1,10 @@
 const reverseString = string => string.split('').reverse().join('');
 const capitalize = (string) =>  string[0].toUpperCase() + string.slice(1);
-const sum = (a, b) => a + b;
+const calculator =  {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b
+  };
 
-export {reverseString, capitalize, sum}
+export {reverseString, capitalize, calculator}
--- functions/functions.test.js
+++ functions/functions.test.js
@@ -1,4 +1,4 @@
-import {reverseString, capitalize, sum} from './functions';
+import {reverseString, capitalize, sum, calculator} from './functions';
 
 test('should Capitalize the string', () => {
     expect(capitalize('word')).toBe('Word');
@@ -9,5 +9,16 @@ expect(reverseString('word')).toBe('drow');
 });
 
 test('1 + 2 should equal 3', () => {
-expect(sum(1, 2)).toBe(3);
+    expect(calculator.add(1, 2)).toBe(3);
 });
+
+test('3 - 1 should equal 2', () => {
+    expect(calculator.subtract(3, 1)).toBe(2);
+});
+
+test('4/2 to equal 2', () => {
+    expect(calculator.divide(4, 2)).toBeCloseTo(2);
+})
+test('4*2 to equal 2', () => {
+    expect(calculator.multiply(4, 2)).toBe(8);
+})
